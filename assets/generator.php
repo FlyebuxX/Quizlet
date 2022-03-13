95abb80344b025ede90106913cdfa6a1498e061d
FlyebuX <valentin.eberhardt57@gmail.com>
2022-03-13 19:19:27 +0100

    Updating documentation & removing redundant code snippets

--- assets/generator.php
+++ assets/generator.php
@@ -2,14 +2,7 @@
 
 require 'assets/new_word.php';  // importing a new word
 
-if (!isset($_GET['answer'])) {  // first visit on the website
-
-    $current_word = word_choice();  // choosing a new word
-    $word_to_guess = explode(":", $current_word)[0];
-    $translation = explode(":", $current_word)[1];
-    file_put_contents('last_words.txt', $word_to_guess . ':' . $translation);
-
-} else {  // the website has already been visited
+if (isset($_GET['answer'])) {  // the website has already been visited
     
     require 'assets/get_update.php';  // updating vars
 
@@ -18,15 +11,13 @@ if (!isset($_GET['answer'])) {  // first visit on the website
     } else {
         $is_found = FALSE;
     }
-    $correct_translation = $last_translation;
-
-    $current_word = word_choice();  // choosing a new word
-
-    $word_to_guess = explode(":", $current_word)[0];
-    $translation = explode(":", $current_word)[1];
-
-    file_put_contents('last_words.txt', $word_to_guess . ':' . $translation);
-
 }
+
+// all cases
+$current_word = word_choice();  // choosing a new word
+$word_to_guess = explode(":", $current_word)[0];
+$translation = explode(":", $current_word)[1];
+
+file_put_contents('last_words.txt', $word_to_guess . ':' . $translation);
 
 ?>
